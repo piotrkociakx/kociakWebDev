863d06f2f2fae104f533ed54b039506d8d72a04e
piotrkociakx <piotrekstypczynski@gmail.com>
2024-08-19 12:55:08 +0200

    #2 commit
    cos tam dodane cos usuniete

--- kociakWeb/src/main/java/pl/kociak/website.backend/App.java
+++ kociakWeb/src/main/java/pl/kociak/website.backend/App.java
@@ -1,13 +1,13 @@
-package pl.kociak.licenseManager;
+package pl.kociak.website.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LicenseManagerApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LicenseManagerApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
